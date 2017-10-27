b09aa1cb66379523df62fca8a253cd21a1f49ff1
yasutomo <ystmkayo@gmail.com>
2017-10-28 01:39:53 +0900

    Use GLSurfaceView
    create class super GLSurfaceView

---
+++ app/src/main/java/com/example/openglgraphic/MainGLView.java
@@ -0,0 +1,19 @@
+package com.example.openglgraphic;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+
+public class MainGLView extends GLSurfaceView {
+
+    private MainRenderer mRenderer;
+
+    public MainGLView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setEGLContextClientVersion(2);
+        mRenderer = new MainRenderer();
+        setRenderer(mRenderer);
+    }
+}
